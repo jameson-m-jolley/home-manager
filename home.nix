5026fd42f79b907cb89573e4675dd6141f0f6a92
jameson-m-jolley <jameson.m.jolley@gmail.com>
2026-02-26 17:10:37 -0700

    auto commit from sh on Thu Feb 26 05:10:37 PM MST 2026 ls -> . .. apply.sh flake.lock flake.nix .git home.nix vpns

--- home.nix
+++ home.nix
@@ -48,12 +48,6 @@ nixpkgs.config.allowUnfree = true;
     openssl
     zlib 
   ];
-  
- home.sessionVariables = {
-  # This tells Lisp (and everything else) where to find C-libraries
-  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.openssl pkgs.zlib ]}";
-}; 
-
 
 #services.flatpak.enable = true;
 #environment.systemPackages = [ pkgs.flatpak-builder ];
