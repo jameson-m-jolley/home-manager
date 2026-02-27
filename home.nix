c67411899a2fc77b20e12f96514c459441014959
jameson-m-jolley <jameson.m.jolley@gmail.com>
2026-02-26 18:12:28 -0700

    auto commit from sh on Thu Feb 26 06:12:28 PM MST 2026 ls -> . .. apply.sh flake.lock flake.nix .git home.nix vpns

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@ nixpkgs.config.allowUnfree = true;
     prismlauncher
     coreutils # basic GNU utilities
     fd
-    zig 
+    (zig.override { version = "0.15.1"; })
     gcc
     clang-tools
     neofetch
@@ -51,11 +51,6 @@ nixpkgs.config.allowUnfree = true;
 
 #services.flatpak.enable = true;
 #environment.systemPackages = [ pkgs.flatpak-builder ];
-
-programs.languages.zig = {
-    enable = true;
-    version = "0.15.1";
-  };
 
 
   # Example program configuration (e.g., for Zsh)
